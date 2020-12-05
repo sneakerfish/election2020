c11102882a59c7820624e79501e5f288df32cb3c
Richard Morello <richard@sneakerfish.com>
2020-12-05 17:59:52 -0500

    Added get_house_results.csv
    Added a file to get the house results.  Also updated the
    R markdown to get US regions.

---
+++ get_house_results.py
@@ -0,0 +1,120 @@
+import os, csv, re, requests, time
+from bs4 import BeautifulSoup
+
+def states():
+    return { 'Alabama': 'AL',
+             'Alaska': 'AK',
+             'American Samoa': 'AS',
+             'Arizona': 'AZ',
+             'Arkansas': 'AR',
+             'California': 'CA',
+             'Colorado': 'CO',
+             'Connecticut': 'CT',
+             'Delaware': 'DE',
+             'District of Columbia': 'DC',
+             'Florida': 'FL',
+             'Georgia': 'GA',
+             'Guam': 'GU',
+             'Hawaii': 'HI',
+             'Idaho': 'ID',
+             'Illinois': 'IL',
+             'Indiana': 'IN',
+             'Iowa': 'IA',
+             'Kansas': 'KS',
+             'Kentucky': 'KY',
+             'Louisiana': 'LA',
+             'Maine': 'ME',
+             'Maryland': 'MD',
+             'Massachusetts': 'MA',
+             'Michigan': 'MI',
+             'Minnesota': 'MN',
+             'Mississippi': 'MS',
+             'Missouri': 'MO',
+             'Montana': 'MT',
+             'Nebraska': 'NE',
+             'Nevada': 'NV',
+             'New Hampshire': 'NH',
+             'New Jersey': 'NJ',
+             'New Mexico': 'NM',
+             'New York': 'NY',
+             'North Carolina': 'NC',
+             'North Dakota': 'ND',
+             'Northern Mariana Islands':'MP',
+             'Ohio': 'OH',
+             'Oklahoma': 'OK',
+             'Oregon': 'OR',
+             'Pennsylvania': 'PA',
+             'Puerto Rico': 'PR',
+             'Rhode Island': 'RI',
+             'South Carolina': 'SC',
+             'South Dakota': 'SD',
+             'Tennessee': 'TN',
+             'Texas': 'TX',
+             'Utah': 'UT',
+             'Vermont': 'VT',
+             'Virgin Islands': 'VI',
+             'Virginia': 'VA',
+             'Washington': 'WA',
+             'West Virginia': 'WV',
+             'Wisconsin': 'WI',
+             'Wyoming': 'WY'
+    }
+
+def parse_district_name(district_name):
+    # Return state-code and number (or 0 for At-Large)
+    m = re.match("(.*)'s? At-large district", district_name)
+    state, district = None, None
+    if m:
+        state = states()[m.group(1)]
+        district = 0
+    else:
+        m = re.match("(.*)'s (\d+)(\w+) district", district_name)
+        if m:
+            state = states()[m.group(1)]
+            district = m.group(2)
+    return state, district
+
+def house_one_state(state_name, csv_writer):
+    url = "https://www.politico.com/2020-election/results/{}/house/".format(state_name)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    tables = soup.find_all(class_="jsx-3350511208 leaderboard")
+    for i in range(len(tables)):
+        table = tables[i]
+        district = table.find(class_="heading").text
+        rows = table.find_all("tr")
+
+        # The first row is a header
+        vals = {}
+        for j in range(1, len(rows)):
+            cols = rows[j].find_all("td")
+            # There are always two columns, candidate name/party and percent raw votes
+            lname = cols[0].find(class_="candidate-short-name").text
+            party = cols[0].find(class_="party-label").text
+            pct = cols[1].find(class_="candidate-percent-only").text
+            num = cols[1].find(class_="candidate-votes-next-to-percent").text
+            # Some of these races are single party.  Only keep the winner, which
+            # is the first listed.
+            if "{}:num".format(party) not in vals:
+                vals["{}:candidate".format(party)] = lname
+                vals["{}:num".format(party)] = int(num.replace(",", ""))
+                vals["{}:pct".format(party)] = float(pct.replace("%", ""))
+        state, distno = parse_district_name(district)
+        csvwriter.writerow([district, state, distno,
+                            vals.setdefault("dem:candidate", ""),
+                            vals.setdefault("gop:candidate", ""),
+                            vals.setdefault("dem:num", 0),
+                            vals.setdefault("gop:num", 0),
+                            vals.setdefault("dem:pct", 0),
+                            vals.setdefault("gop:pct", 0)])
+
+
+if __name__ == "__main__":
+    fname = "2020-house.csv"
+    with open(fname, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for state in states().keys():
+            time.sleep(0.5)
+            print("State: {}".format(state))
+            state_url = state.lower().replace(" ", "-")
+            house_one_state(state_url, csvwriter)
